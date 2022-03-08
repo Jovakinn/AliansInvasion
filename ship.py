79b22fef44fbf76d5c406f5cd74190138680be02
Jovakinn <max05012004@gmail.com>
2022-03-08 22:08:12 +0200

    Initial commit

---
+++ ship.py
@@ -0,0 +1,29 @@
+import pygame
+
+
+class Ship:
+    def __init__(self, ai_game):
+        self.screen = ai_game.screen
+        self.settings = ai_game.settings
+        self.screen_rect = ai_game.screen.get_rect()
+        self.image = pygame.image.load('images/ship.bmp')
+        self.rect = self.image.get_rect()
+        self.x = float(self.rect.x)
+        self.rect.midbottom = self.screen_rect.midbottom
+        self.moving_right = False
+        self.moving_left = False
+        self.ship_speed = 1.5
+
+    def update(self):
+
+        if self.moving_right:
+            self.x += self.settings.ship_speed
+        if self.moving_left:
+            self.x -= self.settings.ship_speed
+        if self.moving_right and self.rect.right < self.screen_rect.right:
+            self.rect.x = self.x
+        if self.moving_left and self.rect.left > 0:
+            self.x -= self.settings.ship_speed
+
+    def blitme(self):
+        self.screen.blit(self.image, self.rect)
